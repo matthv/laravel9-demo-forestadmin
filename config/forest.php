83416a8a463ef63f2a49ef615c70b67519c3ff86
Matt <matthv@gmail.com>
2022-03-09 11:08:41 +0100

    chore: conf development

---
+++ config/forest.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'models_directory' => env('MODEL_DIRECTORY', 'app/Models/'),
+    'models_namespace' => env('MODEL_NAMESPACE', 'App\Models\\'),
+    'json_file_path'   => env('JSON_FILE_PATH', '.forestadmin-schema.json'),
+    'api'              => [
+        'url'         => env('FOREST_URL', 'https://api.development.forestadmin.com'),
+        'secret'      => env('FOREST_ENV_SECRET'),
+        'auth-secret' => env('FOREST_AUTH_SECRET'),
+    ]
+];
